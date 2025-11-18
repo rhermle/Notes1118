49f4f9104c7e2e00870db1c0a9677f7824610820
Ryan Hermle <rhermle@gmail.com>
2025-11-18 15:42:02 -0800

    one last commit

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 using namespace sf;
 
+
+
 class RainbowScreen : public Drawable
 {
 public:
@@ -13,6 +15,28 @@ private:
 	VertexArray m_va; //array of pixel locations and their colors (draw pixels on the screen)
 	Vector2i m_pixel_size;  //size of screen 
 };
+
+void RainbowScreen::update()
+{
+	int pixelHeight = m_pixel_size.y;
+	int pixelWidth = m_pixel_size.x;
+
+	for (int i = 0; i < pixelHeight; i++)
+	{
+		//pick a random color for each row and draw it across the entire row
+		//going to kind of look like a rainbow thingy sorta
+		Uint8 r, g, b; //unsigned char
+		r = rand() % 256;
+		g = rand() % 256;
+		b = rand() % 256;
+		for (int j = 0; j < pixelWidth; j++)
+		{
+			m_va[i * pixelWidth + j].position = { (float)j, (float)i };
+			m_va[i * pixelWidth + j].color = { r,g,b };
+		}
+
+	}
+}
 
 RainbowScreen::RainbowScreen(int pixelWidth, int pixelHeight)
 {
@@ -33,6 +57,9 @@ int main()
 	VideoMode vm(pixelWidth, pixelHeight);
 	RenderWindow window(vm, "Rainbow Screen", Style::Default);
 
+	RainbowScreen screen(pixelWidth, pixelHeight);
+	bool update = true; //only update initially or when they left click
+
 	//Each iteration of this loop is 1 frame
 	while (window.isOpen())
 	{
@@ -45,6 +72,7 @@ int main()
 			{
 				if (event.mouseButton.button == sf::Mouse::Left)
 				{
+					update = true;
 					cout << "Left mouse button was pressed" << endl;
 					cout << "Mouse x: " << event.mouseButton.x << endl;
 					cout << "Mouse y: " << event.mouseButton.y << endl;
@@ -58,16 +86,24 @@ int main()
 		}
 
 		///Update
-		float radius = 150.0;
-		CircleShape circle(150);
-		circle.setFillColor(Color::Magenta);
-		circle.setPosition(960, 540);
-		circle.setOrigin(radius, radius);
+		
+		if (update)
+		{
+			screen.update();
+			update = false;
+		}
+		
+		//float radius = 150.0;
+		//CircleShape circle(150);
+		//circle.setFillColor(Color::Magenta);
+		//circle.setPosition(960, 540);
+		//circle.setOrigin(radius, radius);
 
 		///Draw
 		window.clear();  //clear previous frame
 		//window.draw(stuff);
-		window.draw(circle);
+		///window.draw(circle);
+		window.draw(screen);
 		window.display(); //draw all the things we told it to draw
 	}
-}*/
+}
